c19b9f0229f27d633c1d10d88a5f55467d5808c2
GxPanda <gx.panda@proton.me>
2026-06-20 21:14:34 +0200

    Add workspace.library definition files.

---
+++ main.lua
@@ -0,0 +1,7 @@
+-- This is your main project file. It will be ran every time the item is created.
+
+-- Type definitions file: (optional but recommended)
+workspace.library = { ".pcm/definitions" }
+-- Your code starts now
+
+print("Hello World!")
