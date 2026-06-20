c5e6a4174ecbe711dca96ce7804e5f8ecdc74472
GxPanda <gx.panda@proton.me>
2026-06-20 21:56:22 +0200

    Change http:// to https:// in main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
 -- This is your main project file. It will be ran every time the item is created.
--- Framework by PCS Studio http://gxpanda.xyz/about/pcs.php
+-- Framework by PCS Studio https://gxpanda.xyz/about/pcs.php
 
 print("Hello World!")
