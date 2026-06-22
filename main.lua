8b133e21800123db29231027847721a4c5d47d98
GxPanda <gx.panda@proton.me>
2026-06-22 19:21:09 +0200

    Add helper comment

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
 -- This is your main project file. It will be ran every time the item is created.
+-- Change your project info in .pcm/project.json
+
 -- Framework by PCS Studio https://gxpanda.xyz/about/pcs.php
 
 print("Hello World!")
